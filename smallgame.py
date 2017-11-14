1acc27a574ba174c43dc51d2447ed5d31dc63a40
Ian Mao <yibin.mao@tufts.edu>
2017-11-14 11:33:33 -0500

    the bee is moving but not updating the map correctly

--- smallgame.py
+++ smallgame.py
@@ -17,11 +17,38 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+KeyLookup = {
+    pygame.K_LEFT: "LEFT",
+    pygame.K_RIGHT: "RIGHT",
+    pygame.K_DOWN: "DOWN",
+    pygame.K_UP: "UP"
+}
+
 class MapTile(object):
     def __init__(self, Name, Column, Row):
         self.Name = Name
         self.Column = Column
         self.Row = Row
+
+class Bee(object):
+    def __init__(self, Row, Col):
+        self.Row = Row
+        self.Col = Col
+
+    def move(self, Direction):
+        if Direction == "UP":
+            if self.Row > 0:
+                self.Row -= 1
+        elif Direction == "LEFT":
+            if self.Col > 0:
+                self.Col -= 1
+        elif Direction == "RIGHT":
+            if self.Col < MapSize-1:
+                self.Col += 1
+        elif Direction == "DOWN":
+            if self.Row < MapSize-1:
+                self.Row += 1
+        Map.update()
 
 class Map(object):
     Grid = []
@@ -43,17 +70,30 @@ class Map(object):
         tile = MapTile("flower", randomRow, randomCol)
         Grid[randomRow][randomCol] = tile
 
+    RandomRow = random.randint(0, MapSize - 1)      #Dropping the bee in
+    RandomColumn = random.randint(0, MapSize - 1)
+    Bee = Bee(RandomColumn, RandomRow)
+
+    def update(self):
+        self.Grid[self.Bee.Col][self.Bee.Row] = MapTile("bee", self.Bee.Col, self.Bee.Row)
+
+
 Map = Map()
 
 while not Done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Done = True
+        elif event.type == pygame.KEYDOWN:
+            Map.Bee.move(KeyLookup[event.key])
+
     for Row in range(MapSize):           # Drawing grid
         for Column in range(MapSize):
             if Map.Grid[Column][Row].Name == "flower":
                 Color = RED
-            else:
+            elif Map.Grid[Column][Row].Name == "bee":
+                Color = BLUE
+            else: 
                 Color = WHITE
             pygame.draw.rect(Screen, Color, [(TileMargin + TileWidth) * Column + TileMargin,
                                              (TileMargin + TileHeight) * Row + TileMargin,
